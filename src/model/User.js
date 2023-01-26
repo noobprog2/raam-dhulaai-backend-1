6984435962f53bf807268024bc455d5d969f098e
gaurab77 <gaurabraya69@gmail.com>
2023-01-26 21:29:32 +0545

    jwt

--- src/model/User.js
+++ src/model/User.js
@@ -10,13 +10,22 @@ const userSchema = new mongoose.Schema({
     }, 
     email: {
         type : String ,
-        required: true
+        required: true,
+        unique: true
     },
     phone: String, 
     password: {
         type: String,
         required :true,
+        minlength: 6
     },
+    avatar:{
+        type:String
+    },
+    date:{
+        type:Date,
+        default: Date.now
+    }
 })
 
 
--- src/routes/user_route.js
+++ src/routes/user_route.js
@@ -2,51 +2,79 @@ const express  = require("express");
 const router = express.Router();
 const User = require("../model/User")
 const bodyParser = require("body-parser")
+const gravatar = require('gravatar');   
+const{ check, validationResult}= require ('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+router.post(
+    '/',
+    check('name', 'Name is required').notEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { name, email, password } = req.body;
 
-
-
-router.get("/" , async(req , res) => {
-
-    try{
-        const user = await User.find();
-        res.json({user});
-    }catch (e){
-        res.json({message : e});
-    }
-    res.send("get req hit on user");
-})
-
-router.post("/" , async (req , res) => {
-
-    const user = new User({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
-        phone: req.body.phone
-
-    });
-
-    try{
+      try {
+        let user = await User.findOne({ email });
+  
+        if (user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'User already exists' }] });
+        }
+  
+        const avatar = 
+          gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+          });
+          
+      
+  
+        user = new User({
+          name,
+          email,
+          avatar,
+          password
+        });
+  
+        const salt = await bcrypt.genSalt(10);
+  
+        user.password = await bcrypt.hash(password, salt);
+  
         await user.save();
-        res.json({user});
-    }catch (e){
-        res.json({message: e});
+  
+        const payload = {
+            user: {
+              id: user.id
+            }
+          };
+    
+          jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: '5 days' },
+            (err, token) => {
+              if (err) throw err;
+              res.json({ token });
+            }
+          );
+    }  catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
     }
-
-
-    console.log(req.body);    
-});
-
-router.get("/:name" , async(req ,res) => {
-
-
-    try{
-        const user = await User.find({ name : req.params.name});
-        res.json(user);
-    }catch (e){
-        res.json({message: e});
     }
+      
 
-})
+);
 
 module.exports = router;
