c43ff4fa9da773be9bf2df9784a54478d6d3f0b2
Gaurab2000 <gaurabrayamajhi.075@kathford.edu.np>
2023-01-27 16:41:34 +0545

    update user model

--- src/model/User.js
+++ src/model/User.js
@@ -1,22 +1,25 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 // import mongoose, { mongo } from "mongoose"
 
 //practicing 
 const userSchema = new mongoose.Schema({
     name: {
         type: String , 
-        required: true,
+        required: [true,'Please enter your name!']
         
     }, 
     email: {
         type : String ,
-        required: true,
-        unique: true
+        required: [true,'Please enter your email!'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail,'Please provide a valid email']
     },
     phone: String, 
     password: {
         type: String,
-        required :true,
+        required :[true,'Please enter your password!'],
         minlength: 6
     },
     avatar:{
