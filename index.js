9035203da5fb55e974c49b0a48dd872afc4383e5
Gaurab2000 <gaurabrayamajhi.075@kathford.edu.np>
2023-01-28 22:39:23 +0545

    remove mongoose warning

--- index.js
+++ index.js
@@ -3,31 +3,18 @@ const mongoose = require("mongoose");
 const express = require("express");
 const dotenv = require('dotenv');
 const app = express();
-const connect = require("./config/database")
 const CustomErrorHandler = require("./src/middleware/error_handler")
-
-dotenv.config();
-
-
-const uri = process.env.MONGO_URL;
-
-
-
 const route = express.Router();
-connect.connect();
-
 const ashishRouter = require("./config/routes");
 const userRouter = require("./src/routes/user_route");
 const listenToGet = require("./config/server");
 const bodyParser = require("body-parser");
 const authRouter= require("./src/routes/auth_route");
 app.use(bodyParser.json());
-
 //app.get('/ashish' , ashishRouter)
 app.use('/user', userRouter)
 app.use('/auth', authRouter);
 // listenToGet()
-
 app.get("/" , (req , res) => {
     console.log("inside get")
      res.send({
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const dotenv = require('dotenv');
+const mongoose = require("mongoose");
 const app = require('./index');
+const connect = require("./config/database")
 dotenv.config({path: './.env'});
+mongoose.set("strictQuery", false);
+const uri = process.env.MONGO_URL;
+connect.connect();
+
 const port = process.env.PORT ||9999;
 app.listen(port , ( ) => {
     console.log("Server started on port " + port);
