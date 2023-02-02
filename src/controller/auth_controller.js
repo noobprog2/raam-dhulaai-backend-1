c0338c06fa539a16b9289fd4a7b3b059e0a9ad2c
Gaurab2000 <gaurabrayamajhi.075@kathford.edu.np>
2023-02-02 16:32:38 +0545

    protect-route

--- src/controller/auth_controller.js
+++ src/controller/auth_controller.js
@@ -1,3 +1,4 @@
+const {promisify} = require('util');
 const jwt = require('jsonwebtoken');
 const User= require('./../model/User');
 const catchAsync = require ('./../util/catchAsync');
@@ -15,8 +16,8 @@ const signToken = id => {
           name: req.body.name,
           email: req.body.email,
           password: req.body.password,
-          passwordConfirm: req.body.passwordConfirm 
-           
+          passwordConfirm: req.body.passwordConfirm ,
+          passwordChangedAt: req.body.passwordChangedAt
         });
         const token = signToken(newUser.id) ;
         
@@ -59,3 +60,35 @@ const signToken = id => {
                     token
                 });
         });
+
+exports.protect = catchAsync(async (req,res,next) => {
+    // 1) getting token and check if it's there
+    let token;
+      if(req.headers.authorization && req.headers.authorization.startsWith('Bearer') ){
+         token = req.headers.authorization.split(' ')[1];
+      }
+     
+
+     if(!token){
+        return next(new AppError('You are not logged in!Please log in to get access.',401));
+     }
+    //2) verification of  the token 
+const decoded=await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+
+    // 3) check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if(!currentUser){
+        return next(new AppError('The user belonging to this token  does no longer exist.',401));
+    }
+    //4)check if user change password after the token was issued 
+   if(currentUser.changedPasswordAfter(decoded.iat)){
+    return next(
+        new AppError(' User recentlty changed password! Please log in again',401)
+    );
+   }
+   //grant access to protected route 
+   req.user = currentUser;
+
+    next();
+});
